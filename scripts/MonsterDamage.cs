38c477029e06d53a6f37f0be63da41952fd3344d
anatomicdolly <83445250+anatomicdolly@users.noreply.github.com>
2025-07-14 12:54:08 -0500

    Add files via upload

---
+++ scripts/MonsterDamage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterDamage : MonoBehaviour
+{
+    public int damage;
+    public PlayerHealth playerHealth;
+
+    private void OncollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerHealth.TakeDamage(damage);
+        }
+    }
+}
